9a2a90a7e55a89eef94b9b620534fe75c1719082
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-04-17 16:08:49 +0600

    login pafge design complete

--- src/Authentication/Login/Login.js
+++ src/Authentication/Login/Login.js
@@ -1,9 +1,26 @@
-import React from 'react';
-
+import React, { useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const Login = () => {
+  useEffect(() => {
+    document.title = "Login | Diligent Developer";
+    document.body.style.background = "#2b2f32";
+  }, []);
   return (
     <div>
-      <h1 className='text-center text-warning my-5'>Login</h1>
+      <h1 className="text-center text-warning my-5">Login</h1>
+      <Form className="w-50 mx-auto">
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label className="text-white">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" required/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label className="text-white">Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" required/>
+        </Form.Group>
+        <p className="text-white">Don't have any account?<Link className="text-warning ms-2 text-decoration-none" to= '/register'>Please Register</Link></p>
+        <Button className="font-weight-bolder w-25 mx-auto d-block" variant="warning">Login</Button>
+      </Form>
     </div>
   );
 };
